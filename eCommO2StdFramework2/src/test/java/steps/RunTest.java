0a5548f81bbb6ea5f49f17d9379c6acab5dd107b
VMalalu1 <VMalalu1@LASST219941.uk.pri.o2.com>
2017-03-27 12:27:33 +0100

    adas

--- eCommO2StdFramework2/src/test/java/steps/RunTest.java
+++ eCommO2StdFramework2/src/test/java/steps/RunTest.java
@@ -12,7 +12,7 @@ import cucumber.api.junit.Cucumber;
 	    glue={"steps"},
 		//features = "src/test/java/features/SimFreeDeviceRemoveValidation",
 		plugin = {"pretty", "html:target/cucumber-html-report"},
-		tags = {"@First"}
+		tags = {"@MostRecommended-CFA"}
 		)
 public class RunTest
 {
